aa1f3aa38888327f483c09c21542d7a56e3c1695
Isaac <contact@isaacdpierce.com>
2018-04-30 22:42:08 -0700

    Initialize repository

---
+++ index.js
@@ -0,0 +1,25 @@
+// reverses a string - because js does not have a reverse string built in method.
+// Reverses a string.
+// function reverse(string) {
+//   return Array.from(string).reverse().join("");
+// }
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
